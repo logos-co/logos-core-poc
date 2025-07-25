091e54177b4d914f2d4968a6575bf024b067c476
Iuri Matias <iuri.matias@gmail.com>
2025-07-25 12:40:04 -0400

    feat(modules/logos_irc): Logos IRC Module to proxy Waku to IRC
    feat(modules/logos_irc): Logos IRC Module to proxy Waku to IRC
    
    logos irc module
    
    get message history and bridge
    
    bridge when joining channel and fetch history
    
    support multiple channels
    
    update metadata

---
+++ modules/logos_irc/logos_irc_plugin.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <QtCore/QObject>
+#include <QtCore/QJsonArray>
+#include <QtCore/QStringList>
+#include "logos_irc_interface.h"
+#include "../../SDK/cpp/logos_api.h"
+#include "../../SDK/cpp/logos_api_client.h"
+#include "ircserver.h"
+
+class LogosIRCPlugin : public QObject, public LogosIRCInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID LogosIRCInterface_iid FILE "metadata.json")
+    Q_INTERFACES(LogosIRCInterface PluginInterface)
+
+public:
+    LogosIRCPlugin();
+    ~LogosIRCPlugin();
+
+    Q_INVOKABLE bool foo(const QString &bar) override;
+    QString name() const override { return "logos_irc"; }
+    QString version() const override { return "1.0.0"; }
+
+    // LogosAPI initialization
+    Q_INVOKABLE void initLogos(LogosAPI* logosAPIInstance);
+
+private slots:
+    void onIRCChannelJoined(const QString& channel);
+    void onIRCMessageSent(const QString& channel, const QString& nick, const QString& message);
+
+private:
+    void initChatBridge();
+    void onChatMessage(const QString& eventName, const QVariantList& data);
+    void onHistoryMessage(const QString& eventName, const QVariantList& data);
+    
+    LogosAPI* logosAPI = nullptr;
+    IRCServer* ircServer = nullptr;
+    QObject* chatObject = nullptr;
+    QStringList joinedChannels;
+
+signals:
+    // for now this is required for events, later it might not be necessary if using a proxy
+    void eventResponse(const QString& eventName, const QVariantList& data);
+}; 
